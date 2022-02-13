f7250fd2fcfcb9fed1d91bd7e678e53183c9dd71
Z <ouhassel@gmail.com>
2022-02-13 17:31:47 -0500

    accuracy

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var date2 = new Date("02/12/2022");
 var timed = date1.getTime() - date2.getTime(); 
 var dayd = timed / (1000 * 3600 * 24);
 
-if(dayd>=2){
-    document.getElementById('counter').innerHTML = Math.floor(dayd) + ' DAYS SOBER';
+if(dayd-1>=2){
+    document.getElementById('counter').innerHTML = Math.floor(dayd)-1 + ' DAYS SOBER';
 }
 else{
-    document.getElementById('counter').innerHTML = Math.floor(dayd) + ' DAY SOBER';
+    document.getElementById('counter').innerHTML = Math.floor(dayd)-1 + ' DAY SOBER';
 }
   
