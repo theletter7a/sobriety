1d9fe8390dd5f0b4823001b8e5f2b6fedc948eb2
Z <ouhassel@gmail.com>
2022-02-13 17:33:20 -0500

    accuracy part 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var date2 = new Date("02/12/2022");
 var timed = date1.getTime() - date2.getTime(); 
 var dayd = timed / (1000 * 3600 * 24);
 
-if(dayd-1>=2){
+if(dayd-1>=2||dayd-1<1){
     document.getElementById('counter').innerHTML = Math.floor(dayd)-1 + ' DAYS SOBER';
 }
 else{
